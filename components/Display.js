c951814b1ee97b38e5ab9c36fd84048fe395073c
Hussain-Ahmed989 <hpn423@uregina.ca>
2022-11-29 15:50:19 -0600

    updated display

--- components/Display.js
+++ components/Display.js
@@ -1,6 +1,5 @@
 import { StyleSheet, Text, View, TextInput, Image, TouchableOpacity, ImageBackground, ScrollView, FlatList } from 'react-native'
 import React,{ useState } from 'react'
-import { data } from '../Data';
 
 
 const Display = ({navigation}) => {
